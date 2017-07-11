06734dc08cc48337120fb49f62b7084259aa158d
bobsira <sbobfitz2@gmail.com>
2017-07-11 14:17:16 +0300

    Overriding Methods in a Generic Class

--- src/chp14/Gen1.java
+++ src/chp14/Gen1.java
@@ -4,8 +4,14 @@ package chp14;
  * Created by bobsira on 7/10/17.
  * A subclass of Gen
  */
-public class Gen1<T> extends Gen {
+public class Gen1<T> extends Gen<T> {
     Gen1(T ob) {
         super(ob);
+    }
+
+    // Override getob()
+    // Return ob.
+    T getOb() {
+        return ob;
     }
 }
---
+++ src/chp14/OverrideDemo.java
@@ -0,0 +1,12 @@
+package chp14;
+
+/**
+ * Created by bobsira on 7/11/17.
+ * Demonstrate generic method override
+ */
+public class OverrideDemo {
+    public static void main(String[] args) {
+
+        
+    }
+}
