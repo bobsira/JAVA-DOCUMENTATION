be58f0c74b91ec97c0cc324fd310ac20ad0076e0
bobsira <sbobfitz2@gmail.com>
2017-07-15 16:08:04 +0300

    The LinkedHashSet Class

--- src/chp18/HashSetDemo.java
+++ src/chp18/HashSetDemo.java
@@ -1,15 +1,22 @@
 package chp18;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 /**
  * Created by bobsira on 7/15/17.
+ * HashSet creates a collection that
+ * uses a hash table for storage.
+ *
+ * LinkedHashSet maintains a linked list of the entries in the set, in the order in which
+ * they were inserted.
  */
 public class HashSetDemo {
     public static void main(String[] args) {
         // Create a hash set
         HashSet<String> hashSet = new HashSet<String>();
-        
+
+
         // Add elements to the hash Set.
         hashSet.add("Beta");
         hashSet.add("Alpha");
@@ -19,5 +26,21 @@ public class HashSetDemo {
         hashSet.add("Omega");
 
         System.out.println(hashSet);
+
+
+        // Create a linked hash set
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<String>();
+
+
+
+        // Add elements to the hash Set.
+        linkedHashSet.add("Beta");
+        linkedHashSet.add("Alpha");
+        linkedHashSet.add("Eta");
+        linkedHashSet.add("Gamma");
+        linkedHashSet.add("Epsilon");
+        linkedHashSet.add("Omega");
+
+        System.out.println(linkedHashSet);
     }
 }
