bc07bfd7881698c9c157dc0ab18c5277552bacb0
bobsira <sbobfitz2@gmail.com>
2017-07-19 11:41:40 +0300

    entrySet method example

---
+++ src/chp18/HashMapEntrySetExample.java
@@ -0,0 +1,34 @@
+package chp18;
+
+import javafx.scene.effect.SepiaTone;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by bobsira on 7/19/17.
+ * This example source code demonstrates the use of
+ * entrySet() method of the HashMap class
+ */
+public class HashMapEntrySetExample {
+    public static void main(String[] args) {
+        // initialize the HashMap object with Integer as key and String as value
+        HashMap<Integer, String> mapEmployee = new HashMap<Integer, String>();
+
+        // populate the student map
+        mapEmployee.put(1287, "Aquilino Pimentel");
+        mapEmployee.put(3125, "Travis Davis");
+        mapEmployee.put(9972, "Marianne Laste");
+
+        // get the keys
+        Set<Map.Entry<Integer, String>> mappingSet = mapEmployee.entrySet();
+        System.out.println("Initial value of keys: " + mappingSet);
+
+        // add another key value mapping
+        mapEmployee.put(4581, "Kate Visor");
+
+        // print the values which the set contains
+        System.out.println("New set of keys:" + mappingSet);
+    }
+}
