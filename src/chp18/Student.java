364e232e1d49994ece83780a35437f6eb8533db0
bobsira <sbobfitz2@gmail.com>
2017-07-19 14:20:16 +0300

    Customizing your comparison class

--- src/chp18/Student.java
+++ src/chp18/Student.java
@@ -1,9 +1,26 @@
 package chp18;
 
+
+import java.util.Comparator;
+
 /**
  * Created by bobsira on 7/19/17.
  */
-public class Student {
+public class Student implements Comparable<Student> {
+    public static Comparator<Student> idComperator = new Comparator<Student>() {
+
+        @Override
+        public int compare(Student st1, Student st2) {
+            return (int) (st1.getId() - st2.getId());
+        }
+    };
+    public static Comparator<Student> currentYearComperator = new Comparator<Student>() {
+
+        @Override
+        public int compare(Student st1, Student st2) {
+            return (int) (st1.getCurrentYearOfStudy() - st2.getCurrentYearOfStudy());
+        }
+    };
     private int id;
     private String name;
     private int currentYearOfStudy;
@@ -24,5 +41,16 @@ public class Student {
 
     public int getCurrentYearOfStudy() {
         return currentYearOfStudy;
+    }
+
+    @Override
+    public String toString() {
+        return "[id=" + this.id + ", name=" + this.name
+                + ", Current Year Of Study=" + this.currentYearOfStudy + "]";
+    }
+
+    @Override
+    public int compareTo(Student stud) {
+        return (this.id - stud.id);
     }
 }
