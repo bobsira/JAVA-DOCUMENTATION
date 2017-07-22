14e118da17cca72df0d68a37ab133d7b25ca4dd8
bobsira <sbobfitz2@gmail.com>
2017-07-22 07:02:17 +0300

    Demonstrate a two-dimensional array

---
+++ src/chp3/TwoDArray.java
@@ -0,0 +1,23 @@
+package chp3;
+
+/**
+ * Created by bobsira on 7/22/17.
+ */
+public class TwoDArray {
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][5];
+        int i, j, k = 0;
+
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 5; j++) {
+                twoD[i][j] = k;
+                k++;
+            }
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++)
+                System.out.print(twoD[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
