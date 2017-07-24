859aaf3144f7a52e740b3cca8ebcda4809c383f3
bobsira <sbobfitz2@gmail.com>
2017-07-24 18:34:44 +0300

    Using the comma

---
+++ src/chp5/Sample.java
@@ -0,0 +1,17 @@
+package chp5;
+
+/**
+ * Created by bobsira on 7/24/17.
+ */
+public class Sample {
+    public static void main(String[] args) {
+        int a, b;
+
+        b = 4;
+        for (a = 1; a < b; a++) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            b--;
+        }
+    }
+}
