bce97eb62f10a3eab6082fc2c8671d9cfde62704
bobsira <sbobfitz2@gmail.com>
2017-07-24 18:08:05 +0300

    Demonstrate the for loop

---
+++ src/chp5/ForTick.java
@@ -0,0 +1,13 @@
+package chp5;
+
+/**
+ * Created by bobsira on 7/24/17.
+ */
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+
+        for (n = 10; n > 0; n--)
+            System.out.println("tick " + n);
+    }
+}
