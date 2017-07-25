440cee098af1acaec7a8dc467138cff91c23d80e
bobsira <sbobfitz2@gmail.com>
2017-07-25 11:45:20 +0300

    Using break with nested loops

---
+++ src/chp5/BreakLoop3.java
@@ -0,0 +1,18 @@
+package chp5;
+
+/**
+ * Created by bobsira on 7/25/17.
+ */
+public class BreakLoop3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("pass " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) break; //terminate loop if j is 10
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Loops completed");
+    }
+}
