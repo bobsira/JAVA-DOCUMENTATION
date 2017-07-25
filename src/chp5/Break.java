c6e4cf95e5081048de05e703e01adfab185d78e1
bobsira <sbobfitz2@gmail.com>
2017-07-25 19:49:21 +0300

    Using break as a civilized form of goto

---
+++ src/chp5/Break.java
@@ -0,0 +1,25 @@
+package chp5;
+
+/**
+ * Created by bobsira on 7/25/17.
+ */
+public class Break {
+    public static void main(String[] args) {
+        boolean t = true;
+
+        first:
+        {
+            second:
+            {
+                third:
+                {
+                    System.out.println("Before the break.");
+                    if (t) break second; // break out of second block
+                    System.out.println("This won't execute");
+                }
+                System.out.println("This won't execute");
+            }
+            System.out.println("This is after second block.");
+        }
+    }
+}
