c20d239c863c67b1d98297c4d98affec72d8c339
bobsira <sbobfitz2@gmail.com>
2017-07-25 23:38:08 +0300

    This program includes a method inside the box class.

--- src/chp6/Box.java
+++ src/chp6/Box.java
@@ -7,4 +7,10 @@ public class Box {
     double width;
     double height;
     double depth;
+
+    //    display volume of a box
+    public void volume() {
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
 }
