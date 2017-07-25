7dc6f7a00b737930978044b82b2698197a9b95d8
bobsira <sbobfitz2@gmail.com>
2017-07-25 22:28:54 +0300

    Demonstrate return

--- src/chp5/Return.java
+++ src/chp5/Return.java
@@ -13,4 +13,10 @@ public class Return {
 
         System.out.println("This won't execute");
     }
+    /**
+     * One last point: In the preceding program, the if(t) statement is necessary. Without it, the
+     * Java compiler would flag an “unreachable code” error because the compiler would know that
+     * the last println( ) statement would never be executed. To prevent this error, the if statement is
+     * used here to trick the compiler for the sake of this demonstration.
+     */
 }
