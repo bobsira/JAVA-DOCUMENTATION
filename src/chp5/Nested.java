7a7457022a7a5834f2431c345d0d9ee90a41d36c
bobsira <sbobfitz2@gmail.com>
2017-07-25 11:27:52 +0300

    Loops may be nested

--- src/chp5/Nested.java
+++ src/chp5/Nested.java
@@ -8,7 +8,7 @@ public class Nested {
         int i, j;
 
         for (i = 0; i < 10; i++) {
-            for (j = 1; j < 10; j++)
+            for (j = i; j < 10; j++)
                 System.out.print(".");
             System.out.println();
         }
