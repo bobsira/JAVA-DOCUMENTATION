03c63ef654ff400c6b123d62d0b454212f3463ef
bobsira <sbobfitz2@gmail.com>
2017-07-25 19:55:52 +0300

    Using break to exit from nested loops

---
+++ src/chp5/BreakLoop4.java
@@ -0,0 +1,19 @@
+package chp5;
+
+/**
+ * Created by bobsira on 7/25/17.
+ */
+public class BreakLoop4 {
+    public static void main(String[] args) {
+        outer:
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Pass " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) break outer; // exit both loops
+                System.out.print(j + " ");
+            }
+            System.out.println("This will not print");
+        }
+        System.out.println("Loop complete");
+    }
+}
