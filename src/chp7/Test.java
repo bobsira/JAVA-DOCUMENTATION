d3654e9ae255e47c9ca1efa1b78e1e1493999c58
bobsira <sbobfitz2@gmail.com>
2017-07-26 18:38:14 +0300

    Primitive types are passed by value

--- src/chp7/Test.java
+++ src/chp7/Test.java
@@ -11,9 +11,18 @@ public class Test {
         b = j;
     }
 
+    Test() {
+
+    }
+
     // return true if o is equal to the invoking object
     boolean equalTo(Test o) {
         if (o.a == a && o.b == b) return true;
         else return false;
+    }
+
+    void meth(int i, int j) {
+        i *= 2;
+        j /= 2;
     }
 }
