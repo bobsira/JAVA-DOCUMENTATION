bed2dd6be23b534bb50b8082a9d620b20920f074
bobsira <sbobfitz2@gmail.com>
2017-07-26 19:16:01 +0300

    Another example that uses recursion

---
+++ src/chp7/RecTest.java
@@ -0,0 +1,19 @@
+package chp7;
+
+/**
+ * Created by bobsira on 7/26/17.
+ */
+public class RecTest {
+    int values[];
+
+    RecTest(int i) {
+        values = new int[i];
+    }
+
+    // display array --recursively
+    void printArray(int i) {
+        if (i == 0) return;
+        else printArray(i - 1);
+        System.out.println("[" + (i - 1) + "] " + values[i - 1]);
+    }
+}
--- src/chp7/Recursion.java
+++ src/chp7/Recursion.java
@@ -5,10 +5,16 @@ package chp7;
  */
 public class Recursion {
     public static void main(String[] args) {
-        Factorial factorial = new Factorial();
 
+
+        Factorial factorial = new Factorial();
         System.out.println("Factorial of 3 is " + factorial.fact(3));
         System.out.println("Factorial of 4 is " + factorial.fact(4));
         System.out.println("Factorial of 5 is " + factorial.fact(5));
+
+        RecTest ob = new RecTest(10);
+        int i;
+        for (i = 0; i < 10; i++) ob.values[i] = i;
+        ob.printArray(10);
     }
 }
