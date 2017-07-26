7ebc50537704bdf51acb6e51bf299b130f69404f
bobsira <sbobfitz2@gmail.com>
2017-07-26 11:24:07 +0300

    This class defines an integer stack that can hold 10 values

--- src/chp6/Stack.java
+++ src/chp6/Stack.java
@@ -5,26 +5,23 @@ package chp6;
  * This class defines an integer stack that can hold 10 values.
  */
 public class Stack {
-    int stck[]=new int[10];
+    int stck[] = new int[10];
     int tos;
-
     // Initialize top-of-stack
-    Stack(){
-        tos=-1;
+    Stack() {
+        tos = -1;
     }
-
     // Push an item onto the stack
-    void push(int item){
-        if (tos==9)
-            System.out.println("Stack overflow");
+    void push(int item) {
+        if (tos == 9)
+            System.out.println("Stack is full.");
         else
-            stck[++tos]=item;
+            stck[++tos] = item;
     }
-
     // Pop an item from the stack
-    int pop(){
-        if (tos<0){
-            System.out.println("Stack underflow");
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Stack underflow.");
             return 0;
         }
         else
--- src/chp6/TestStack.java
+++ src/chp6/TestStack.java
@@ -5,20 +5,17 @@ package chp6;
  */
 public class TestStack {
     public static void main(String[] args) {
-        Stack myStack1=new Stack();
-        Stack myStack2=new Stack();
-
-        //push some numbers onto the stack
-        for (int i=0;i<10;i++) myStack1.push(i);
-        for (int i=0;i<10;i++) myStack2.push(i);
-
-        //pop those numbers off the stack
-        System.out.println("Stack in myStack1:");
-        for (int i=0;i<10;i++)
-            System.out.print(myStack1.pop()+" ");
-
-        System.out.println("\nStack in myStack2:");
-        for (int i=0;i<10;i++)
-            System.out.print(myStack2.pop()+" ");
+        Stack mystack1 = new Stack();
+        Stack mystack2 = new Stack();
+// push some numbers onto the stack
+        for (int i = 0; i < 10; i++) mystack1.push(i);
+        for (int i = 10; i < 20; i++) mystack2.push(i);
+// pop those numbers off the stack
+        System.out.print("Stack in mystack1: ");
+        for (int i = 0; i < 10; i++)
+            System.out.print(mystack1.pop() + " ");
+        System.out.print("\nStack in mystack2: ");
+        for (int i = 0; i < 10; i++)
+            System.out.print(mystack2.pop() + " ");
     }
 }
