ad288a5cd8df0b8489ce3f17c28134a99fcc400b
bobsira <sbobfitz2@gmail.com>
2017-07-27 11:58:02 +0300

    Display all command-line arguments

---
+++ src/chp7/CommandLine.java
@@ -0,0 +1,11 @@
+package chp7;
+
+/**
+ * Created by bobsira on 7/27/17.
+ */
+public class CommandLine {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+    }
+}
