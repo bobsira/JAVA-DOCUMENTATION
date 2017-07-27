22d089fb8234491891f9da6d1ba3a9772b990dc9
bobsira <sbobfitz2@gmail.com>
2017-07-27 10:26:18 +0300

    This class defines an integer stack that can hold 10 values

---
+++ src/chp7/Stack.java
@@ -0,0 +1,37 @@
+package chp7;
+
+/**
+ * Created by bobsira on 7/26/17.
+ */
+public class Stack {
+    /**
+     * Now, both stck and tos are private. This means
+     * that they cannot be accidentally or maliciously
+     * altered in a way that would be harmful to the stack
+     */
+
+    private int stck[] = new int[10];
+    private int tos;
+
+    // Initialize the top of the stack
+    Stack() {
+        tos = -1;
+    }
+
+    // push an item onto the stack
+    void push(int item) {
+        if (tos == 9)
+            System.out.println("Stack is full");
+        else
+            stck[++tos] = item;
+    }
+
+    // pop an item from the stack
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Stack underflow");
+            return 0;
+        } else
+            return stck[tos--];
+    }
+}
