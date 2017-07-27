084f90cca41b85427b46c0437bde04594c062550
bobsira <sbobfitz2@gmail.com>
2017-07-27 11:15:13 +0300

    Define an inner class within a for loop

--- src/chp7/InnerClassDemo.java
+++ src/chp7/InnerClassDemo.java
@@ -7,5 +7,6 @@ public class InnerClassDemo {
     public static void main(String[] args) {
         Outer outer = new Outer();
         outer.test();
+        outer.show();
     }
 }
--- src/chp7/Outer.java
+++ src/chp7/Outer.java
@@ -6,6 +6,18 @@ package chp7;
 class Outer {
     int outer_x = 100;
 
+    void show() {
+        for (int i = 0; i < 10; i++) {
+            class Inner {
+                void display() {
+                    System.out.println("display: outer_x = " + outer_x);
+                }
+            }
+            Inner inner = new Inner();
+            inner.display();
+        }
+    }
+
     void test() {
         Inner inner = new Inner();
         inner.display();
