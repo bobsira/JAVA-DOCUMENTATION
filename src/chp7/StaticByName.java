00f4a42148781debfdd2b431568f3ef4bdb9d720
bobsira <sbobfitz2@gmail.com>
2017-07-27 10:41:03 +0300

    static keyword demonstration

---
+++ src/chp7/StaticByName.java
@@ -0,0 +1,11 @@
+package chp7;
+
+/**
+ * Created by bobsira on 7/27/17.
+ */
+public class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
+    }
+}
---
+++ src/chp7/StaticDemo.java
@@ -0,0 +1,14 @@
+package chp7;
+
+/**
+ * Created by bobsira on 7/27/17.
+ */
+public class StaticDemo {
+
+    static int a = 42;
+    static int b = 99;
+
+    static void callme() {
+        System.out.println("a = " + a);
+    }
+}
