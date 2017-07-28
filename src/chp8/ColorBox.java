cd4e2d8e565a566151462f608ed5470141d5a101
bobsira <sbobfitz2@gmail.com>
2017-07-28 21:34:06 +0300

    Here, Box is extended to include color

---
+++ src/chp8/ColorBox.java
@@ -0,0 +1,15 @@
+package chp8;
+
+/**
+ * Created by bobsira on 7/28/17.
+ */
+public class ColorBox extends Box {
+    int color; // color of box
+
+    ColorBox(double width, double height, double depth, int color) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.color = color;
+    }
+}
