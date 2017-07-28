964367b57c1117569ee1e7f8223d0caec83ab8c1
bobsira <sbobfitz2@gmail.com>
2017-07-28 15:19:15 +0300

    Inheritance Basics

---
+++ src/chp8/A.java
@@ -0,0 +1,13 @@
+package chp8;
+
+/**
+ * Created by bobsira on 7/28/17.
+ * create a superclass
+ */
+public class A {
+    int i, j;
+
+    void showij() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
---
+++ src/chp8/B.java
@@ -0,0 +1,17 @@
+package chp8;
+
+/**
+ * Created by bobsira on 7/28/17.
+ * Create a subclass by extending class A
+ */
+public class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+
+    void sum() {
+        System.out.println("i+j+k: " + (i + j + k));
+    }
+}
