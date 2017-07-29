fe44bf7e6ba7b9d04247e73c6794884dd10415ab
bobsira <sbobfitz2@gmail.com>
2017-07-29 22:57:29 +0300

    Methods with differing type signatures are overloaded – not overridden.

--- src/chp8/Override.java
+++ src/chp8/Override.java
@@ -7,6 +7,7 @@ public class Override {
     public static void main(String[] args) {
         B subOb = new B(1, 2, 3);
 
+        subOb.show("This is k: "); // this calls show() in B
         subOb.show(); // this calls show() in B
     }
 }
