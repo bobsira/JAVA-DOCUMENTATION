e20f81729ff1387d4f5cf5eecd700e2bffc83b54
bobsira <sbobfitz2@gmail.com>
2017-07-30 07:59:31 +0300

    A Short Package Example

---
+++ src/chp9/MyPack/Balance.java
@@ -0,0 +1,24 @@
+package chp9.MyPack;
+
+/**
+ * Created by bobsira on 6/12/17.
+ */
+/* Now, the Balance class, its constructor, and its
+show() method are public. This means that they can
+be used by non-subclass code outside their package.
+*/
+public class Balance {
+    private String name;
+    private double bal;
+
+    public Balance(String name, double bal) {
+        this.name = name;
+        this.bal = bal;
+    }
+
+    public void show() {
+        if (bal < 0)
+            System.out.print("--> ");
+        System.out.println(name + ": $" + bal);
+    }
+}
