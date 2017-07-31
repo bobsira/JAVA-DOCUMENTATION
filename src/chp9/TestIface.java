cf3af17a0a0db5552153919f9f522a147edf1296
bobsira <sbobfitz2@gmail.com>
2017-07-31 11:19:02 +0300

    Accessing Implementations Through Interface References

---
+++ src/chp9/TestIface.java
@@ -0,0 +1,12 @@
+package chp9;
+
+
+/**
+ * Created by bobsira on 6/12/17.
+ */
+class TestIface {
+    public static void main(String[] args) {
+        Callback c = new Client();
+        c.callback(42);
+    }
+}
