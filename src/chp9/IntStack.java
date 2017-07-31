21af7fce54ce758b3838aefc7a6d241bece10e3f
bobsira <sbobfitz2@gmail.com>
2017-07-31 11:56:44 +0300

    Define an integer stack interface

---
+++ src/chp9/IntStack.java
@@ -0,0 +1,11 @@
+package chp9;
+
+/**
+ * Created by bobsira on 6/13/17.
+ * Define an integer stack interface.
+ */
+public interface IntStack {
+    void push(int item); //store an item
+
+    int pop(); //retrieve an item
+}
