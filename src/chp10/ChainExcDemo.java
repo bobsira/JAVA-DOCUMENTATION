6e2b2ff42d44c208c1b3bf41688e085a5dc7d125
bobsira <sbobfitz2@gmail.com>
2017-08-03 01:39:04 +0300

    Chained Exceptions

---
+++ src/chp10/ChainExcDemo.java
@@ -0,0 +1,30 @@
+package chp10;
+
+/**
+ * Created by bobsira on 8/3/17.
+ * Demonstrating exception chaining
+ */
+public class ChainExcDemo {
+    static void demoproc(){
+
+        // create an exception
+        NullPointerException e = new NullPointerException("top layer");
+
+        // add a cause
+        e.initCause( new ArithmeticException("cause"));
+
+        throw e;
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoproc();
+        } catch (NullPointerException e){
+            // display top level exception
+            System.out.println("Caught: " + e);
+
+            // display cause exception
+            System.out.println("Original cause: " + e.getCause());
+        }
+    }
+}
