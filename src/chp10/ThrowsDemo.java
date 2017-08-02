8bf8e4213194be39bfb6062691b8bdbe5b5b8cb4
bobsira <sbobfitz2@gmail.com>
2017-08-02 23:55:31 +0300

    throws example

--- src/chp10/ThrowsDemo.java
+++ src/chp10/ThrowsDemo.java
@@ -4,12 +4,16 @@ package chp10;
  * Created by bobsira on 8/2/17.
  */
 public class ThrowsDemo {
-    static void throwOne(){
+    static void throwOne() throws IllegalAccessException{
         System.out.println("Inside throwOne.");
         throw new IllegalAccessException("demo");
     }
 
     public static void main(String[] args) {
-        throwOne();
+        try {
+            throwOne();
+        } catch (IllegalAccessException e) {
+            System.out.println("Caught " + e);
+        }
     }
 }
