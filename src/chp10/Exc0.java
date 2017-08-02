4045203040cb17c01f20f13ab8aa9ef0e7d71476
bobsira <sbobfitz2@gmail.com>
2017-08-02 09:19:53 +0300

    Uncaught Exceptions

---
+++ src/chp10/Exc0.java
@@ -0,0 +1,11 @@
+package chp10;
+
+/**
+ * Created by bobsira on 8/2/17.
+ */
+public class Exc0 {
+    public static void main(String[] args) {
+        int d = 0;
+        int a = 42 /0;
+    }
+}
