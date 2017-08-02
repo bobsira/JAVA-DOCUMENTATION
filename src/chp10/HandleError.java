f0b7b7f28d3fe435bfffff3e8b8a2b0a79d50b11
bobsira <sbobfitz2@gmail.com>
2017-08-02 12:39:21 +0300

    Handle an exception and move on

---
+++ src/chp10/HandleError.java
@@ -0,0 +1,25 @@
+package chp10;
+
+import java.util.Random;
+
+/**
+ * Created by bobsira on 8/2/17.
+ */
+public class HandleError {
+    public static void main(String[] args) {
+        int a=0, b=0, c=0;
+        Random random = new Random();
+
+        for (int i=0; i<32000; i++){
+            try {
+                b = random.nextInt();
+                c = random.nextInt();
+                a = 12345 / (b/c);
+            } catch (ArithmeticException e){
+                System.out.println("Division by zero.");
+                a = 0; // set a to zero and continue
+            }
+            System.out.println("a: " +a);
+        }
+    }
+}
