3c77ffe4ed2bd647d8edb79a1d2ed4c83310f1a2
bobsira <sbobfitz2@gmail.com>
2017-08-02 13:08:02 +0300

    Reverse will now work fine

--- src/chp10/SuperSubCatch.java
+++ src/chp10/SuperSubCatch.java
@@ -14,15 +14,12 @@ public class SuperSubCatch {
         try {
             int a =0;
             int b = 42/ a;
+        }
+        catch(ArithmeticException e){ // ERROR - unreachable
+            System.out.println("This is never reached");
         } catch (Exception e){
             System.out.println("Generic Exception catch.");
         }
-        /**
-         * This catch is never reached because
-         * ArithmeticException is a subclass of Exception
-         */
-        catch(ArithmeticException e){ // ERROR - unreachable
-            System.out.println("This is never reached");
-        }
+
     }
 }
