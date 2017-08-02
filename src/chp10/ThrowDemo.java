7516d6f9db016b5b90a6f887f15d71c3da23c713
bobsira <sbobfitz2@gmail.com>
2017-08-02 23:42:01 +0300

    throw

---
+++ src/chp10/ThrowDemo.java
@@ -0,0 +1,24 @@
+package chp10;
+
+/**
+ * Created by bobsira on 8/2/17.
+ * Demonstrate throw
+ */
+public class ThrowDemo {
+    static void demoproc(){
+        try {
+            throw new NullPointerException("demo");
+        } catch (NullPointerException e){
+            System.out.println("Caught inside demoproc.");
+            throw e; // rethrow the exception
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoproc();
+        } catch (NullPointerException e){
+            System.out.println("Recaught: " + e);
+        }
+    }
+}
