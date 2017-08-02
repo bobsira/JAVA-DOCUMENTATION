d55da303f885929a0eb7af21d6ac5f6b2d6cf5f2
bobsira <sbobfitz2@gmail.com>
2017-08-02 11:15:16 +0300

    Try and catch block

---
+++ src/chp10/Exc2.java
@@ -0,0 +1,18 @@
+package chp10;
+
+/**
+ * Created by bobsira on 8/2/17.
+ */
+public class Exc2 {
+    public static void main(String[] args) {
+        int d, a;
+
+        try { // monitor a block of code
+            d = 0;
+            a = 42 / d;
+            System.out.println("This will not be printed");
+        } catch (ArithmeticException e ){ // catch divide-by-zero error
+            System.out.println("Division by zero.");
+        }
+    }
+}
