f046b558a590e1761aa48fa3badcc909e05ae92c
bobsira <sbobfitz2@gmail.com>
2017-08-07 10:43:26 +0300

    Extending Thread

--- src/chp11/Newthread.java
+++ src/chp11/Newthread.java
@@ -2,6 +2,26 @@ package chp11;
 
 /**
  * Created by bobsira on 8/4/17.
+ * Create a second thread by extending Thread
  */
-public class Newthread {
+public class Newthread extends Thread{
+    Newthread(){
+        // Create a new, second thread
+        super("Demo Thread");
+        System.out.println("Child thread: " + this);
+        start();  // Start the thread
+    }
+
+    // This is the entry point for the second thread.
+    public void rum(){
+        try {
+            for ( int i =5; i >0; i--){
+                System.out.println("Child Thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e){
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
 }
