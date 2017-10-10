18efb0591605b40a68719e570098b4f15ac47058
bobsira <sbobfitz2@gmail.com>
2017-10-10 12:00:08 +0300

    Use a quantifier

--- src/chp30/RegExpr0.java
+++ src/chp30/RegExpr0.java
@@ -4,5 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExpr0 {
-
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("W+");
+        Matcher matcher = pattern.matcher("W WW WW");
+        while (matcher.find())
+            System.out.println("Match: " + matcher.group());
+    }
 }
