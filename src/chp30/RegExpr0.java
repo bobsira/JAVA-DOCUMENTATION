0935e23cf12a95eb7ae9c21c990de4d51b93262a
bobsira <sbobfitz2@gmail.com>
2017-10-10 12:18:57 +0300

    Use the ? quantifier

--- src/chp30/RegExpr0.java
+++ src/chp30/RegExpr0.java
@@ -9,5 +9,33 @@ public class RegExpr0 {
         Matcher matcher = pattern.matcher("W WW WW");
         while (matcher.find())
             System.out.println("Match: " + matcher.group());
+
+        System.out.println();
+        /**
+         * Use wildcard and quantifier
+         */
+        Pattern pattern1 = Pattern.compile("e.+d");
+        Matcher matcher1 = pattern1.matcher("extend cup end table");
+        while (matcher1.find())
+            System.out.println("Match: " + matcher1.group());
+        /**
+         * output
+         * Match: extend cup end
+         *
+         * The reason that the longer sequence is found is that, by default, fnd() matches the longest sequence that fits
+         * the pattern. This is called greedy behavior.
+         *
+         * You can specify reluctant behavior by adding the ? quantifier to the pattern
+         */
+
+        /**
+         * Use the ? quantifier
+         */
+        System.out.println();
+        // use reluctant matching behavior.
+        Pattern pattern2 = Pattern.compile("e.+?d");
+        Matcher matcher2 = pattern2.matcher("extend cup end table");
+        while (matcher2.find())
+            System.out.println("Match: " + matcher2.group());
     }
 }
